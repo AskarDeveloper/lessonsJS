1c4999d067ccfbeab14d25a4f4528df9ada12670
Askar <it.web.cs@gmail.com>
2019-03-19 01:39:56 +0600

    fixed

--- lesson5/task2/js/main.js
+++ lesson5/task2/js/main.js
@@ -1,23 +1,19 @@
 'use strict';
 let startBtn = document.getElementById('start'),
-  budget = document.getElementsByClassName('budget-value'),
-  dayBudget = document.getElementsByClassName('daybudget-value'),
-  level = document.getElementsByClassName('level-value'),
-  expenses = document.getElementsByClassName('expenses-value'),
-  optionalExpenses = document.getElementsByClassName('optionalexpenses-value'),
-  income = document.getElementsByClassName('income-value'),
-  monthSavings = document.getElementsByClassName('monthsavings-value'),
-  yearSavings = document.getElementsByClassName('yearsavings-value'),
+  budgetValue = document.getElementsByClassName('budget-value')[0],
+  dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+  levelValue = document.getElementsByClassName('level-value')[0],
+  expensesValue = document.getElementsByClassName('expenses-value')[0],
+  optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+  incomeValue = document.getElementsByClassName('income-value')[0],
+  monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+  yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
   expensesInput = document.getElementsByClassName('expenses-item'),
-
-  allButtons = document.getElementsByTagName('button'),
-  approveFirstBtn = allButtons[0],
-  approveSecondBtn = allButtons[1],
-  calculateBtn = allButtons[2],
-
+  expensesBtn = document.getElementsByTagName('button')[0],
+  optionalExpensesBtn = document.getElementsByTagName('button')[1],
+  countBtn = document.getElementsByTagName('button')[2],
   optionalExpensesInput = document.querySelectorAll('.optionalexpenses-item'),
-
   choosIncomeInput = document.querySelector('.choose-income'),
   checkbox = document.querySelector('#savings'),
   chooseSumInput = document.querySelector('.choose-sum'),
@@ -25,3 +21,5 @@ let startBtn = document.getElementById('start'),
   yearValueInput = document.querySelector('.year-value'),
   monthValueInput = document.querySelector('.month-value'),
   dayValueInput = document.querySelector('.day-value');
+
+  console.log(budgetValue);
